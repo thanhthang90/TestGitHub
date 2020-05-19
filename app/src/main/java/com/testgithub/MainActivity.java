67f7044c9eb92ee6734cdc28d557b05c817309ce
Tô thanh Thắng <thanhthang90@gmail.com>
2020-05-19 15:39:29 +0700

    test 2

--- app/src/main/java/com/testgithub/MainActivity.java
+++ app/src/main/java/com/testgithub/MainActivity.java
@@ -1,8 +1,11 @@
 package com.testgithub;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bSecond = findViewById(R.id.bSecond);
+        bSecond.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), SecondActivity.class));
+            }
+        });
     }
 }
